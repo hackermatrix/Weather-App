047be7d5272c8ed02bb24c560daa2fc338fddc3d
Hrishikesh Jadhav <popeye@localhost.localdomain>
2023-12-13 21:21:47 +0530

    all fixed

--- src/compo/CurrentInfoCard.jsx
+++ src/compo/CurrentInfoCard.jsx
@@ -1,110 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import '../App.css'
-import axios from 'axios';
-
-
-const getData = async ()=>{
-  const response = await axios.get("https://api.weatherapi.com/v1/forecast.json?q=Ahmednagar%2Cin&days=1&hour=5&key=5cadec3d7211419eb0c64922231312");
-  return response.data
-}
-
-const getHourData = async ()=>{
-  let dateobj = new Date();
-  let apidate = `${dateobj.getFullYear()}-${dateobj.getMonth()}-${dateobj.getDay()}`;
-  const response = await axios.get(`https://api.weatherapi.com/v1/history.json?q=Ahmednagar%2Cin&dt=${apidate}&key=5cadec3d7211419eb0c64922231312`);
-  return response.data
-}
-
-
-
-
-
+import { useEffect, useState } from "react";
+import "../App.css";
+import axios from "axios";
 
 function CurrentInfoCard() {
-  const [data,setData] = useState(null);
-  const [hourlyData,setHourlyData] = useState(null);
-  const times = [9,12,15,18,21];
+  const [data, setData] = useState(null);
+  const [hourlyData, setHourlyData] = useState(null);
+  const times = [9, 12, 15, 18, 21];
 
   useEffect(() => {
     const fetchData = async () => {
-      setData(await getData());
-      setHourlyData(await getHourData());
+      await getData();
+      await getHourData();
     };
 
     fetchData();
   }, []);
 
-  
-  console.log(data);
+  const getData = async () => {
+    const response = await axios.get(
+      "https://api.weatherapi.com/v1/forecast.json?q=Ahmednagar%2Cin&days=1&hour=5&key=5cadec3d7211419eb0c64922231312"
+    );
+    setData(response.data);
+    console.log(response.data);
+  };
+
+  const getHourData = async () => {
+    let dateobj = new Date();
+    let apidate = `${dateobj.getFullYear()}-${
+      dateobj.getMonth() + 1
+    }-${dateobj.getDate()}`;
+    const response = await axios.get(
+      `https://api.weatherapi.com/v1/history.json?q=Ahmednagar%2Cin&dt=${apidate}&key=5cadec3d7211419eb0c64922231312`
+    );
+    setHourlyData(response.data);
+    console.log(response.data);
+  };
 
   return (
-    <div className='info-card'>
-        <section className='inner-current-card'>
-          <h3>Current Weather</h3>
-          <br/>
-          <section style={{display:"flex",justifyContent:'space-around'}}>
-    
-            <span style={{display:'flex',flexDirection:"column"}}>
-              <span><strong>{data.location.name},{data.location.country}</strong></span>
-              <span>{data.location.localtime}</span>
-            </span>
-    
-            <span style={{display:'flex',flexDirection:"column"}}>
-              <span><strong>{data.current.feelslike_c} <sup>o</sup>C</strong></span>
-              <span>{data.current.condition.text}</span>
-            </span>
-    
-            <span>
-              <img src={data.current.condition.icon}></img>
-            </span>
-          </section>
-        </section>
-
-        <section className='inner-current-card'>
-        <h3>Air Conditions</h3>
-        <br/>
-        <section style={{display:"flex",justifyContent:'space-around'}}>
-          
-            <span className='air-condition-card'>
-                <span>Real Feel</span>
-                <span><strong>{data.current.feelslike_c} <sup>o</sup>c</strong></span>
-            </span>
-
-            <span className='air-condition-card'>
-                <span>Wind</span>
-                <span><strong>{data.current.wind_mph} m/s</strong></span>
-            </span>
-
-            <span className='air-condition-card'>
-                <span>UV </span>
-                <span><strong>{data.current.uv} %</strong></span>
-            </span>
-
-            <span className='air-condition-card'>
-                <span>Humidity</span>
-                <span><strong>{data.current.humidity}%</strong></span>
-            </span>
-        </section>
-        </section>
-        <br/>
-
-        <section className='inner-current-card'>
-        <h3>Today's Forecast</h3>
-        <br/>
-        <section style={{display:'flex',justifyContent:"center"}}>
-              {times.map((time)=>
-              <span key="eg" className='today-forecast-card'>
-              <span>{time}:00</span>
-              <span><img  src={hourlyData.forecast.forecastday[0].hour[time].condition.icon}/></span>
-              <span><strong>{hourlyData.forecast.forecastday[0].hour[time].temp_c}<sup>o</sup>C</strong></span>
+    <div className="info-card">
+      {data && (
+        <>
+          <section className="inner-current-card">
+            <h3>Current Weather</h3>
+            <br />
+            <section
+              style={{ display: "flex", justifyContent: "space-around" }}
+            >
+              <span style={{ display: "flex", flexDirection: "column" }}>
+                <span>
+                  <strong>
+                    {data.location.name},{data.location.country}
+                  </strong>
+                </span>
+                <span>{data.location.localtime}</span>
               </span>
-              )}
 
+              <span style={{ display: "flex", flexDirection: "column" }}>
+                <span>
+                  <strong>
+                    {data.current.feelslike_c} <sup>o</sup>C
+                  </strong>
+                </span>
+                <span>{data.current.condition.text}</span>
+              </span>
 
-        </section>
-        </section>
+              <span>
+                <img
+                  src={data.current.condition.icon}
+                  alt="Current Weather Icon"
+                />
+              </span>
+            </section>
+          </section>
+
+          <section className="inner-current-card">
+            <h3>Air Conditions</h3>
+            <br />
+            <section
+              style={{ display: "flex", justifyContent: "space-around" }}
+            >
+              <span className="air-condition-card">
+                <span>Real Feel</span>
+                <span>
+                  <strong>
+                    {data.current.feelslike_c} <sup>o</sup>c
+                  </strong>
+                </span>
+              </span>
+
+              <span className="air-condition-card">
+                <span>Wind</span>
+                <span>
+                  <strong>{data.current.wind_mph} m/s</strong>
+                </span>
+              </span>
+
+              <span className="air-condition-card">
+                <span>UV </span>
+                <span>
+                  <strong>{data.current.uv} %</strong>
+                </span>
+              </span>
+
+              <span className="air-condition-card">
+                <span>Humidity</span>
+                <span>
+                  <strong>{data.current.humidity}%</strong>
+                </span>
+              </span>
+            </section>
+          </section>
+          <br />
+
+          <section className="inner-current-card">
+            <h3>Todays Forecast</h3>
+            <br />
+            <section style={{ display: "flex", justifyContent: "center" }}>
+              {times.map((time) => (
+                <span key={time} className="today-forecast-card">
+                  <span>{time}:00</span>
+                  <span>
+                    <img
+                      src={
+                        hourlyData?.forecast?.forecastday[0]?.hour[time]
+                          ?.condition?.icon
+                      }
+                      alt={`Weather icon at ${time}:00`}
+                    />
+                  </span>
+                  <span>
+                    <strong>
+                      {hourlyData?.forecast?.forecastday[0]?.hour[time]?.temp_c}
+                      <sup>o</sup>C
+                    </strong>
+                  </span>
+                </span>
+              ))}
+            </section>
+          </section>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default CurrentInfoCard
+export default CurrentInfoCard;
--- src/compo/WeeklyInfoCard .jsx
+++ src/compo/WeeklyInfoCard .jsx
@@ -1,92 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import '../App.css'
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function getPrevDates(days){
+function getPrevDates(days) {
   var date = new Date();
-  var last = new Date(date.getTime() - (days * 24 * 60 * 60 * 1000));
-  var day =last.getDate();
-  var month=last.getMonth()+1;
-  var year=last.getFullYear();
-  return `${year}-${month}-${day}`
-  }
-  
-
-
-const getData = async ()=>{
-  try{
-  const response = await axios.get(`https://api.weatherapi.com/v1/history.json?q=Ahmednagar%2Cin&dt=${getPrevDates(6)}&end_dt=${getPrevDates(1)}&key=5cadec3d7211419eb0c64922231312`);
-  return response.data
-}
-  catch (error) {
-    console.log(error);
-    return error;
-  }
-  
-  
+  var last = new Date(date.getTime() - days * 24 * 60 * 60 * 1000);
+  var day = last.getDate();
+  var month = last.getMonth() + 1;
+  var year = last.getFullYear();
+  return `${year}-${month}-${day}`;
 }
 
-
-
-
-function WeeklyInfoCard () {
+function WeeklyInfoCard() {
   return (
-    <section className='info-card'>
-        <h3>Weekly Weather</h3>
-        <WeekCard/>
+    <section className="info-card">
+      <h3>Weekly Weather</h3>
+      <WeekCard />
     </section>
-  )
+  );
 }
-
-
-
-// let data = await getData();
-// let weekdata = data.forecast.forecastday;
 
 function WeekCard() {
+  const [data, setData] = useState(null);
 
-  const [data,setData] = useState(null);
-  useEffect(()=>{
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.weatherapi.com/v1/history.json?q=Ahmednagar%2Cin&dt=${getPrevDates(
+            6
+          )}&end_dt=${getPrevDates(1)}&key=5cadec3d7211419eb0c64922231312`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    const fetchData = async()=>{
-      setData(await getData());
-    }
-    fetchData();
-  },[]);
+    getData();
+  }, []);
 
-  let weekdata = data.forecast.forecastday;
+  const weekdata = data?.forecast?.forecastday || [];
 
   return (
     <>
-      {weekdata.map((day)=>
-        <section key="test"className='week-card'>
+      {weekdata.map((day) => (
+        <section key={day.date_epoch} className="week-card">
+          <section className="inner-week-card">
+            <span>
+              <strong>
+                {new Date(day.date).toLocaleDateString(undefined, {
+                  weekday: "long",
+                })}
+              </strong>
+            </span>
 
-        <section className='inner-week-card'>
-        <span><strong>{new Date(day.date).toLocaleDateString(undefined, { weekday: 'long' })}</strong></span>
+            <span style={{ display: "flex", alignItems: "center" }}>
+              <img
+                style={{ height: "60%" }}
+                src={day.day.condition.icon}
+                alt={`Weather icon for ${day.date}`}
+              />
+              <span>{day.day.condition.text}</span>
+            </span>
+          </section>
 
-        <span style={{display:"flex",alignItems:"center"}}>
-          <img style={{height:"60%"}} src={day.day.condition.icon}/>
-          <span>{day.day.condition.text}</span>
-        </span>
-        
+          <section className="inner-week-card">
+            <span>
+              <strong>
+                {day.day.avgtemp_c}
+                <sup>o</sup>C
+              </strong>
+            </span>
+            <span style={{ marginTop: "40%" }}>
+              <strong>{day.day.uv}%</strong>
+            </span>
+          </section>
+
+          <section className="inner-week-card">
+            <span>
+              <strong>{day.day.maxwind_mph} m/s</strong>
+            </span>
+            <span style={{ marginTop: "40%" }}>
+              <strong>{day.day.avghumidity}%</strong>
+            </span>
+          </section>
         </section>
-  
-        <section className='inner-week-card'>
-          <span><strong>{day.day.avgtemp_c}<sup>o</sup>C</strong></span>
-          <span style={{marginTop:"40%"}}><strong>{day.day.uv}%</strong></span>
-        </section>
-
-        <section className='inner-week-card'>
-          <span><strong>{day.day.maxwind_mph} m/s</strong></span>
-          <span style={{marginTop:"40%"}}><strong>{day.day.avghumidity}%</strong></span>
-        </section>
-  
-      </section>
-
-        )}
-        </>
-
-  )
+      ))}
+    </>
+  );
 }
 
-export default WeeklyInfoCard 
+export default WeeklyInfoCard;
